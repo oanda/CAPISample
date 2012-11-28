f511b6997d4b000d76736e5c74a0d8b4f43de692
Jimmy Sun <jimmys@oanda.com>
2012-11-28 16:56:56 -0500

    Initial commit.

---
+++ APISample.c
@@ -0,0 +1,39 @@
+//
+//  main.c
+//  testhelloc
+//
+//  Created by Jimmy Sun on 2012-11-28.
+//  Copyright (c) 2012 OANDA. All rights reserved.
+//
+
+/** Description
+ *  Sample project in C using libcurl.
+ *  Requests the rate data for EUR/USD, USD/CAD, AND USD/JPY and outputs them to standard output.
+ */
+
+#include <stdio.h>
+#include <curl/curl.h>
+
+int main() {
+    CURL *curl;
+    CURLcode res;
+    
+    curl = curl_easy_init();
+    if(curl) {
+        curl_easy_setopt(curl, CURLOPT_URL,
+                         "http://api-sandbox.oanda.com/v1/instruments/price?instruments=EUR_USD,USD_CAD,USD_JPY");
+        
+        /* Perform the request, res will get the return code */
+        res = curl_easy_perform(curl);
+        
+        /* Check for errors */
+        if(res != CURLE_OK) {
+            fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                    curl_easy_strerror(res));
+        }
+        
+        /* always cleanup */
+        curl_easy_cleanup(curl);
+    }
+    return 0;
+}
---
+++ README.txt
@@ -0,0 +1,13 @@
+### CAPISample
+
+Sample C program using the curl library to fetch rate data for EUR/USD, USD/CAD, and USD/JPY.
+
+### Compiling and Running
+
+Compile the file through terminal using the GCC compiler. Make sure to link the curl library using the **-lcurl** flag.
+> gcc APISample.c -lcurl -o output_file_name
+
+Run the output file using the following command.
+> ./output_file_name
+
+Outputs to standard output.
